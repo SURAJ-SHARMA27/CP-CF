c3f291bb2ef2195037f6e46e06596b35edd4d00f
SURAJ <90304648+SURAJ-SHARMA27@users.noreply.github.com>
2023-11-11 23:56:52 +0530

    kfintech

--- cp26.cpp
+++ cp26.cpp
@@ -1,20 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-int t;
-cin>>t;
-while(t--){
-int a,b;
-cin>>a>>b;
-int sum=2;
-int fl=1;
-int i=1;
-while(sum<a){
-    sum=sum+(b*i);
-    fl++;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Interval {
+    int start, finish;
+} intervals[100];
+
+int output1;
+
+int compare(const void* a, const void* b) {
+    struct Interval *x = (struct Interval*)a, *y = (struct Interval*)b;
+    return x->finish - y->finish;
 }
-cout<<fl<<endl;
+
+int lectureHallScheduling(int input1, int input2[], int input3[]) {
+    int i, last_finished, cnt;
+    
+    for (int i = 0; i < input1; i++) {
+        intervals[i].start = input2[i];
+        intervals[i].finish = input3[i];
+    }
+
+    qsort(intervals, input1, sizeof(struct Interval), compare);
+
+    for (i = last_finished = cnt = 0; i < input1; i++) {
+        if (intervals[i].start >= last_finished) {
+            cnt++;
+            last_finished = intervals[i].finish;
+        }
+    }
+
+    output1 = cnt;
+    return output1;
 }
-return 0;
+
+int main() {
+    // Example usage:
+    int input1 = 3;
+    int input2[] = {3, 2, 6};
+    int input3[] = {5, 8, 9};
+
+    int result = lectureHallScheduling(input1, input2, input3);
+    printf("Maximum number of requests that can be accepted: %d\n", result);
+
+    return 0;
 }
--- cp27.cpp
+++ cp27.cpp
@@ -1,29 +1,29 @@
-string printMinNumberForPattern(string S){
-        // code here 
-          int i = 1;
-        stack<int> st;
-        string result = "";
-        
-        for (char c: S) {
-            st.push(i);
-            i += 1;
-            
-            if (c == 'I') {
-                while (!st.empty()) {
-                    int el = st.top();
-                    st.pop();
-                    result.append(to_string(el));
-                }
-            }
-        }
-        
-        st.push(i);
-        while (!st.empty()) {
-            int el = st.top();
-            st.pop();
-            result.append(to_string(el));
-        }
-        
-        return result;
-        
+#include <iostream>
+
+int modexp(int a, int b, int n) {
+    int result = 1;
+    int base = a % n; // Ensure base is within the modulo range
+    int power = b;
+
+    while (power > 0) {
+        if (power % 2 == 1)
+            result = (result * base) % n;
+
+        base = (base * base) % n;
+        power /= 2; // Integer division by 2
     }
+
+    return result;
+}
+
+int main() {
+    // Example usage:
+    int a = 2;
+    int b = 10;
+    int n = 100000;
+
+    int result = modexp(a, b, n);
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
